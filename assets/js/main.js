816ed2a819a615f3c6ed1cf50f9208290607e23a
SABARIRAJ0207 <sabariraj8838@gmail.com>
2021-12-24 23:52:10 +0530

    Feature: Fix nav

--- assets/js/main.js
+++ assets/js/main.js
@@ -13,6 +13,28 @@ navToggler.addEventListener('click', () => {
 
     navIcon.forEach(icon => {
         icon.classList.toggle('hidden');
-    })
+    });
 
-})
+});
+
+function navbarFixed(){
+   const header_dom = document.querySelector('.header');
+
+   const nav_offset_top = header_dom.clientHeight + 50;
+
+   window.addEventListener("scroll", () => {
+
+    let scroll = window.pageYOffset || document.documentElement.scrollTop;
+   
+    if(scroll >= nav_offset_top ) {
+
+        header_dom.classList.add('navbar-fixed');
+    }else{
+        header_dom.classList.remove('navbar-fixed');
+    }
+    
+    
+   });
+}
+
+navbarFixed();
